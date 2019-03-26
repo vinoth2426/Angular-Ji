7cf43e9b4b474e875396513a2844512857d94a53
Vinoth <35895723+vinoth2426@users.noreply.github.com>
2019-03-26 10:11:05 +0530

    Add files via upload

---
+++ devOpsReport/src/main/webapp/app.js
@@ -0,0 +1,69 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute', 'ngCookies'])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'home/home.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'login/login.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/register', {
+                controller: 'RegisterController',
+                templateUrl: 'register/register.view.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/login' });
+    }
+
+    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+    function run($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+        	alert($rootScope.globals.currentUser.authdata);
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+        
+        $rootScope.directive('datetimez', function() {
+            return {
+                restrict: 'A',
+                require : 'ngModel',
+                link: function(scope, element, attrs, ngModelCtrl) {
+                  element.datetimepicker({
+                    dateFormat:'dd/MM/yyyy hh:mm:ss',
+                    language: 'pt-BR'
+                  }).on('changeDate', function(e) {
+                    ngModelCtrl.$setViewValue(e.date);
+                    scope.$apply();
+                  });
+                }
+            };
+        });
+
+    }
+
+})();
